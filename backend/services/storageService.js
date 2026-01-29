80f3f92b9e28cf9d9107a11feab21239ee053e1b
victor-olamide <olaboyevictor20@gmail.com>
2026-01-29 04:01:53 -0800

    "feat-add-storage-service"

---
+++ backend/services/storageService.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+const uploadToIPFS = async (fileBuffer, fileName) => {
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  
+  let data = new FormData();
+  data.append('file', fileBuffer, {
+    filename: fileName
+  });
+
+  const response = await axios.post(url, data, {
+    headers: {
+      'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+      pinata_api_key: process.env.PINATA_API_KEY,
+      pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+    },
+  });
+
+  return `ipfs://${response.data.IpfsHash}`;
+};
+
+module.exports = {
+  uploadToIPFS,
+};
