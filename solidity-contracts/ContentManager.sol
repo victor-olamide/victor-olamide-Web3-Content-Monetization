9201cdf93c0b0f42db6023da2792fcb569c5c7b3
victor-olamide <olaboyevictor20@gmail.com>
2026-01-30 07:19:49 -0800

    "ContentManager"

---
+++ solidity-contracts/ContentManager.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./interfaces/IContentAccess.sol";
+
+contract ContentManager is IContentAccess {
+    struct Content {
+        address creator;
+        uint256 price;
+    }
+
+    mapping(uint256 => Content) public contents;
+    mapping(address => mapping(uint256 => bool)) public userAccess;
+    uint256 public contentCount;
+
+    function addContent(uint256 price) external override returns (uint256) {
+        uint256 contentId = ++contentCount;
+        contents[contentId] = Content(msg.sender, price);
+        emit ContentAdded(contentId, msg.sender, price);
+        return contentId;
+    }
+
+    function purchaseContent(uint256 contentId) external payable override {
+        Content storage content = contents[contentId];
+        require(content.creator != address(0), "Content not found");
+        require(msg.value >= content.price, "Insufficient payment");
+
+        userAccess[msg.sender][contentId] = true;
+        emit ContentAccessed(contentId, msg.sender);
+    }
+
+    function hasAccess(address user, uint256 contentId) external view override returns (bool) {
+        return userAccess[user][contentId] || contents[contentId].creator == user;
+    }
+}
