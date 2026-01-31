71558d982e9274b40f1cd9e49df68e0cb6ba2d5e
victor-olamide <olaboyevictor20@gmail.com>
2026-01-31 01:24:04 -0800

    "feat-update-models-for-revenue-tracking"

--- backend/models/Purchase.js
+++ backend/models/Purchase.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const purchaseSchema = new mongoose.Schema({
   contentId: { type: Number, required: true },
   user: { type: String, required: true },
+  creator: { type: String, required: true },
   txId: { type: String, required: true, unique: true },
   amount: { type: Number, required: true },
   timestamp: { type: Date, default: Date.now }
