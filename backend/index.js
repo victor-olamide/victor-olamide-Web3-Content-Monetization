5594fa57a280107dc272ddb43fdb08363871c1f4
victor-olamide <olaboyevictor20@gmail.com>
2026-02-13 01:21:18 -0800

    feat(recommendations): mount /api/recommendations routes in app

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ const walletRoutes = require('./routes/walletRoutes');
 const previewRoutes = require('./routes/previewRoutes');
 const webhookRoutes = require('./routes/webhookRoutes');
 const webhookAdminRoutes = require('./routes/webhookAdminRoutes');
+const recommendationRoutes = require('./routes/recommendationRoutes');
 
 app.use('/api/content', contentRoutes);
 app.use('/api/purchases', purchaseRoutes);
@@ -59,6 +60,7 @@ app.use('/api/wallet', walletRoutes);
 app.use('/api/preview', previewRoutes);
 app.use('/api/webhooks', webhookRoutes);
 app.use('/api/webhooks/admin', webhookAdminRoutes);
+app.use('/api/recommendations', recommendationRoutes);
 
 // Start Indexer
 const indexer = require('./services/indexer');
