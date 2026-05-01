e2eb3006dca2055d7f92c964840bc85e24b3ed79
victor-olamide <olaboyevictor20@gmail.com>
2026-05-01 07:50:06 -0800

    "FeeManagement"

--- solidity-contracts/CoreMonetization.sol
+++ solidity-contracts/CoreMonetization.sol
@@ -30,6 +30,10 @@ contract CoreMonetization is BaseSubscription, ContentManager, IBaseMonetization
         platformFeeBps = _feeBps;
     }
 
+    function calculateFee(uint256 amount) public view returns (uint256) {
+        return (amount * platformFeeBps) / 10000;
+    }
+
     function setPlatformFee(uint256 feeBps) external override onlyOwner {
         require(feeBps <= 1000, "Fee too high");
         uint256 oldFee = platformFeeBps;
@@ -41,6 +45,20 @@ contract CoreMonetization is BaseSubscription, ContentManager, IBaseMonetization
         uint256 balance = address(this).balance;
         payable(owner).transfer(balance);
         emit FundsWithdrawn(owner, balance);
+    }
+
+    function purchaseContent(uint256 contentId) external payable override {
+        Content storage content = contents[contentId];
+        require(content.creator != address(0), "Content not found");
+        require(msg.value >= content.price, "Insufficient payment");
+
+        uint256 fee = calculateFee(msg.value);
+        uint256 creatorShare = msg.value - fee;
+
+        userAccess[msg.sender][contentId] = true;
+        payable(content.creator).transfer(creatorShare);
+        
+        emit ContentAccessed(contentId, msg.sender);
     }
 
     function getPlatformFee() external view override returns (uint256) {
