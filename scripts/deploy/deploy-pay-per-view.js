3eddb5d74e344c049906735ed55743b030fa043a
victor-olamide <olaboyevictor20@gmail.com>
2026-05-03 07:29:05 -0800

    Add platform wallet configuration to deployment script

--- scripts/deploy/deploy-pay-per-view.js
+++ scripts/deploy/deploy-pay-per-view.js
@@ -1,5 +1,14 @@
 const { deployContract } = require('./utils');
 
-deployContract('pay-per-view')
-  .then(txid => console.log(`TXID: ${txid}`))
+async function deployPayPerView() {
+  console.log('Deploying pay-per-view contract...');
+  const txid = await deployContract('pay-per-view');
+  console.log(`TXID: ${txid}`);
+  console.log('\nIMPORTANT: After deployment, configure platform wallet using:');
+  console.log('  clarinet console');
+  console.log('  (contract-call? .pay-per-view set-platform-wallet <wallet-address>)');
+  return txid;
+}
+
+deployPayPerView()
   .catch(err => console.error(err));
