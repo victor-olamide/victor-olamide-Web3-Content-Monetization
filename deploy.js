71ab07e4d82ff17c199539611c78bdc6153bc472
victor-olamide <olaboyevictor20@gmail.com>
2026-05-21 15:13:19 +0100

    security: load deployer mnemonic from DEPLOYER_MNEMONIC env var instead of hardcoding

--- deploy.js
+++ deploy.js
@@ -21,7 +21,7 @@ const {
 const { STACKS_MAINNET } = require(path.join(STACKS_MODULES, '@stacks/network'));
 const { generateWallet } = require(path.join(STACKS_MODULES, '@stacks/wallet-sdk'));
 
-const MNEMONIC        = 'run ice main distance mass family dust section breeze grab sign diary umbrella then doctor exclude uncover love discover scrap robust crazy click type';
+const MNEMONIC        = process.env.DEPLOYER_MNEMONIC;
 const DEPLOYER        = 'SPG2T28R84NNF0AB8PFQRSGY16CD76KV72YHPADH';
 const NODE_URL        = 'https://api.mainnet.hiro.so';
 const NETWORK         = { ...STACKS_MAINNET, coreApiUrl: NODE_URL };
